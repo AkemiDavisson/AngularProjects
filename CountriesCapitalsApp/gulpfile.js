cb86d2deaaae995538350daf5ee1eb616dbeebab
Akemi Davisson <akemi.davisson@enterpriseinnovations.net>
2014-09-02 14:52:56 -0500

    commit attempt 3

---
+++ CountriesCapitalsApp/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	uglify = require('gulp-uglify'),
+	minifyHtml = require('gulp-minify-html'),
+	minifyCss = require('gulp-minify-css'),
+	usemin = require('gulp-usemin'),
+	rev = require('gulp-rev'),
+	rimraf = require('gulp-rimraf'); //replaces gulp-clean?
+
+gulp.task('default', function() {
+
+});
+
