ca858ba7399cd795d8bd7fd2d4239af233df2aea
Akemi Davisson <akemi.davisson@enterpriseinnovations.net>
2014-09-15 13:53:19 -0500

    finished editable item directive

--- directives-demo/app/scripts/editable-items/editable-directive.js
+++ directives-demo/app/scripts/editable-items/editable-directive.js
@@ -10,12 +10,29 @@ function makeEditable(){
 		restrict: 'A',
 		templateUrl: 'partials/editable-item/makeEditable-template.html',
 		transclude: true,
-		link: function(scope, element, attr) {
-			scope.editableP = element.find('p');
-		}
-		// controller: function($scope, $element, $attr) {
+		link: function(scope, element) {
+			scope.editableDiv = element.find('div');
+		},
+		controller: function($scope, $element) {
+			//set scope of editable false
+			$scope.editable = false;
+			$scope.buttonText = 'Edit';
 
-		// }
+			$scope.toggleEdit = function () {
+
+				//if editable = false switch to true
+				if (!$scope.editable) {
+					$scope.editable = true;
+					$scope.buttonText = 'Save'; //change button text
+					
+					$scope.editableDiv.focus();
+				}
+				else {
+					$scope.editable = false;
+					$scope.buttonText = 'Edit';
+				}
+			}
+		}
 
 	};
 
